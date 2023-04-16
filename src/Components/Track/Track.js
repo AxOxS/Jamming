7ddb02b543dbe3ff670173480d6c2ec97b63977b
AxOxS <axoxs1@gmail.com>
2023-04-16 20:59:03 +0300

    Add + - button / changed the background photo

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,6 +2,14 @@ import React from 'react';
 import './Track.css';
 
 export class Track extends React.Component {
+    renderAction() {
+        if(this.props.isRemoval) {
+            return <button className='Track-action'>-</button>
+        } else {
+            return <button className='Track-action'>+</button>
+        }
+    }
+
     render() {
         return (
             <div className='Track'>
@@ -9,7 +17,8 @@ export class Track extends React.Component {
                     <h3>{/*Track name will go here*/}</h3>
                     <p>{/*Track Artist | track album */}</p>
                 </div>
-                <button className='Track-action'>{/*+ or - will go here*/}</button>
+                {/* <button className='Track-action'>{/*+ or - will go here*/}
+                {this.renderAction()}
             </div>
         );
     }
